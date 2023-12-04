5bac4136bae85bf56b7cd7beedc9c2c8c35d5a10
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2023-12-03 19:29:04 -0500

    Update server.py

--- TMR_v2/server.py
+++ TMR_v2/server.py
@@ -1,5 +1,5 @@
 from xmlrpc.client import boolean
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import random
 import time
 import pandas as pd
@@ -8,29 +8,51 @@ from firebase_admin import credentials, db,firestore
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 
+#Initialize Flask app
+app = Flask(__name__, static_url_path='/static')
+db_instance = None
+data = [] #Defined the data var at the beignning 
 
-app = Flask(__name__)
+# Firebase authentication
+c_path= r"TMRWebScrape-main\TMRWebScrape-main\TMR_v2\tmr-project-8f8de-firebase-adminsdk-8xj0a-4696ee732a.json"
+login=credentials.Certificate(c_path)
+firebase_app=firebase_admin.initialize_app(login)
+db=firestore.client()
 
+# Route to render the main page with data
 @app.route('/')
 def index():
-  return render_template('TMR.html')
+  return render_template('TMR.html', data = data)
 
-@app.route('/', methods=['POST'])
+@app.route('/get_data')
+def get_data():
+    job_info_data = get_job_info_data()
+    return jsonify(job_info_data)
+# Created this function to retrieve job information data from Firebase
+def get_job_info_data():
+    job_info_data = []
+    job_info_collection = db.collection('new_web_data').stream()
+
+    for job_info_doc in job_info_collection:
+        job_info = job_info_doc.to_dict()
+        job_info_data.append(job_info)
+
+    return job_info_data
+
+# Route to handle the web scrapping process
+@app.route('/scraped', methods=['POST'])
 def my_link():
-    #TMR indeed webscraper 
+    #TMR indeed webscraper
+    global data
+    data =[]
     
-    #authenticate the database
-    c_path= r"tmr-project-8f8de-firebase-adminsdk-8xj0a-4696ee732a.json"
-    login=credentials.Certificate(c_path)
-    app=firebase_admin.initialize_app(login)
-    db=firestore.client()
-    
+    # This function will perform a web scrape on the Indeed website
     def getData(url,index)->list:
-        data = []
+    
         #Start web browser driver to emulate a real browser visiting the site to avoid captcha or bot recognition
         browser_options = ChromeOptions()
         driver = Chrome(options=browser_options)
-
+        # This sets the user agent to avoid bot recognition
         driver.execute_cdp_cmd('Emulation.setUserAgentOverride', {
 
                "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
@@ -75,7 +97,7 @@ def my_link():
    
         driver.quit()
         return data
-    
+    # This checks the database to see if the given text is the same as an index in the db
     def check_db(txt)->boolean:#checkls the database if a givinen txt is in the job index returns boolean
         jobs = db.collection('Job Index').stream()
         for job in jobs:
@@ -87,6 +109,7 @@ def my_link():
         print('flase')
         return False
 
+    #This function will count the number of job index's in the database
     def count_db()->int:
         holder=0
         jobs= db.collection('Job Index').stream()
@@ -95,25 +118,26 @@ def my_link():
         return holder+1
 
     def main():
+        global data
         data = []
         text = request.form['text']
         text = text.replace(" ","-")
         doc_ref=db.collection('Job Index').document(text)
         count=0
         index=0
+
         if check_db(text):#check to see if the  search text is already in the database if true get the # of listings 
            new_ref=db.collection('Job Index').stream()
            for job in new_ref:
-               holder=job.to_dict()
+               holder = job.to_dict()
                if holder.get('title')==text:
                  count=holder.get('number of listings')
                  index=holder.get('index')
         else:
             
-            index=count_db()
-
-        #print('count:'+count+'index'+index)
-        for i in range(3):
+            index=count_db()    
+        # This will make the program run (2) times for more data
+        for i in range(2):
             data += getData(("https://www.indeed.com/q-" + text + "-jobs.html"),index)
             count+=len(data)
             #pausing for random amount of time to emulate real user
@@ -131,7 +155,7 @@ def my_link():
 
     main()
 
-    return 'scraped'
+    return render_template('TMR.html', data=data)
 
 if __name__ == '__main__':
   app.run(debug=True)
